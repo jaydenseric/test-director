c0f9f7ba7a2e3ca3172aabab406f7add5f0c65ae
Jayden Seric <me@jaydenseric.com>
2020-03-24 20:02:36 +1100

    Tidy the position of a JSDoc comment.

--- test/sleep.js
+++ test/sleep.js
@@ -1,14 +1,13 @@
 'use strict'
 
-module.exports =
-  /**
-   * Sleeps the process for a specified duration.
-   * @kind function
-   * @name sleep
-   * @param {number} ms Duration in milliseconds.
-   * @returns {Promise<void>} Resolves once the duration is up.
-   * @ignore
-   */
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-  }
+/**
+ * Sleeps the process for a specified duration.
+ * @kind function
+ * @name sleep
+ * @param {number} ms Duration in milliseconds.
+ * @returns {Promise<void>} Resolves once the duration is up.
+ * @ignore
+ */
+module.exports = function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
